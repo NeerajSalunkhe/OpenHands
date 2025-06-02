f68bec44c0ce2f0d7854ac110d563c2ed376817c
neeraj <22je0618@iitism.ac.in>
2025-06-02 08:11:54 +0530

    fifth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,31 +1,31 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import dbConnect from "@/app/lib/dbConnect";
-import User from "@/app/models/user";
-
-await dbConnect(); // top-level await is okay in Next 15+
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import dbConnect from '@/app/lib/dbConnect';
+import User from '@/app/models/user';
 
 const handler = NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      
     }),
   ],
   callbacks: {
     async signIn({ user }) {
-      if (!user.email) return false;
-
+      await dbConnect();
       const existingUser = await User.findOne({ email: user.email });
+
       if (!existingUser) {
         const newUser = new User({
           email: user.email,
-          username: user.email.split("@")[0],
+          username: user.email.split('@')[0],
           profilephoto: user.image,
         });
         await newUser.save();
@@ -34,9 +34,8 @@ const handler = NextAuth({
         user.name = existingUser.username;
       }
       return true;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
+    }
+  }
 });
 
 export { handler as GET, handler as POST };
